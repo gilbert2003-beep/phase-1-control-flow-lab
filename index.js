79a7e2357ed122b43ed8a4c82a281dcb16850904
gilbert <gilbert.bageni@student.moringaschool.com>
2023-06-14 15:07:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return 'This one is on me!';
+    }
+    else if (ride > 2000 && ride <= 2500){
+    return 'I will gladly take your thirty bucks.';
+    }
+    else if (ride > 2500){
+    return 'No can do.';
+    }
+  }
+
+  // Write your code here!}
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+// Use a switch statement to check the tip and return the appropriate response
+switch (tip) {
+case "generous":
+return "Thank you so much.";
+case "not as generous":
+return "Thank you.";
+default:
+return "Bye.";
+}
 }
 
-function switchOnCharmFromTip(){
   // Write your code here!
-}
+
+
